36cfa1141acd675a61af221c30d7f6fdc8cf68b7
Emiliano <Emiliano@EMILIANO-LAPTOP>
2019-04-06 19:32:09 +0000

    First steps

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+  * The entry point.
+  *
+  * @module Afipjs
+  */
+
+module.exports= {
+    Wsaa : require('./lib/wsaa').Wsaa,
+    Wsfe1 : require('./lib/wsfe1').Wsfe1,
+};
